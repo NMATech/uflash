a40a6583e520703ad1b19eca807b60c1a39fb480
NMAtech <nadindramaulanaaziz@gmail.com>
2024-09-15 13:39:56 +0700

    adding theme color

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 function App() {
   return (
     <>
-      
+      <h1 className="font-bold text-uflash-light-text bg-uflash-dark-bg w-full">
+        test
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          text: "#333",
+          primary: "#3b82f6",
+          accent: "#f59e0b",
+          bg: "#f3f4f6",
+        },
+        dark: {
+          text: "#e5e7eb",
+          primary: "#60a5fa",
+          accent: "#fcd34d",
+          bg: "#1f2937",
+        },
+      },
+    },
   },
   plugins: [],
 };
